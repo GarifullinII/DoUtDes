065f759b0060a1e2342852b21b72d8b0fc620227
GarifullinII <ildaridrisovich@gmail.com>
2024-12-09 17:10:58 +0400

    add extensions, image, fonts, view controller
    add UIColor, UIFont and UIStackView extensions, WelcomeViewController, AppIcon, AIcon, change SceneDelegate

---
+++ DoUtDes/Extensions/UIKit/UIColor+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Extensions.swift
+//  DoUtDes
+//
+//  Created by Ildar Garifullin on 09.12.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static let specialGreen = #colorLiteral(red: 0.5242634416, green: 0.8234111071, blue: 0.4243985116, alpha: 1)
+    static let specialBlue = #colorLiteral(red: 0.4963650107, green: 0.5668778419, blue: 0.8650380969, alpha: 1)
+    static let specialOrange = #colorLiteral(red: 0.9972394109, green: 0.4982676506, blue: 0.3419620991, alpha: 1)
+    static let specialYellow = #colorLiteral(red: 1, green: 0.800115943, blue: 0, alpha: 1)
+    static let specialWhiteOne = #colorLiteral(red: 0.9481336474, green: 0.9531062245, blue: 0.9788157344, alpha: 1)
+    static let specialWhiteTwo = #colorLiteral(red: 0.9638200402, green: 0.9687924981, blue: 0.9902083278, alpha: 1)
+    static let specialWhiteThree = #colorLiteral(red: 0.9803918004, green: 0.9803923965, blue: 0.9933025241, alpha: 1)
+    static let specialWhiteFour = #colorLiteral(red: 0.9999999404, green: 1, blue: 1, alpha: 1)
+    static let specialBlackOne = #colorLiteral(red: 0.1873404682, green: 0.1923201978, blue: 0.200824976, alpha: 1)
+    static let specialBlackTwo = #colorLiteral(red: 0.474509418, green: 0.4745100141, blue: 0.4874110222, alpha: 1)
+    static let specialBlackThree = #colorLiteral(red: 0.6326069832, green: 0.6475488544, blue: 0.6901822686, alpha: 1)
+    static let specialBlackFour = #colorLiteral(red: 0.673623383, green: 0.6785958409, blue: 0.6785081029, alpha: 1)
+}
